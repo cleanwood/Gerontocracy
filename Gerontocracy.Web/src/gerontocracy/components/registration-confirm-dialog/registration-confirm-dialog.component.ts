5327fbe2b2eea082d57892c9d8e72de31b35c415
MonZta123 <MonZta123@users.noreply.github.com>
2019-08-17 00:09:21 +0200

    fixed build

--- Gerontocracy.Web/src/gerontocracy/components/registration-confirm-dialog/registration-confirm-dialog.component.ts
+++ Gerontocracy.Web/src/gerontocracy/components/registration-confirm-dialog/registration-confirm-dialog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicDialogRef } from 'primeng/api';
 
 @Component({
   selector: 'app-registration-confirm-dialog',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationConfirmDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogReference: DynamicDialogRef) { }
 
   ngOnInit() {
   }
 
+  closeConfirmation() {
+    this.dialogReference.close();
+  }
 }
