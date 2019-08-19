657a14a2692c0eddcfafb30e702f74c44d93896b
MonZta123 <MonZta123@users.noreply.github.com>
2019-08-19 22:11:41 +0200

    Added Db Migration

--- Gerontocracy.Data/Migrations/20190819200943_Tasks.Designer.cs
+++ Gerontocracy.Data/Migrations/20190819200943_Tasks.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Gerontocracy.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Gerontocracy.Data.Migrations
 {
     [DbContext(typeof(GerontocracyContext))]
-    partial class GerontocracyContextModelSnapshot : ModelSnapshot
+    [Migration("20190819200943_Tasks")]
+    partial class Tasks
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Gerontocracy.Data/Migrations/20190819200943_Tasks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Gerontocracy.Data.Migrations
+{
+    public partial class Tasks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aufgabe",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    TaskType = table.Column<int>(nullable: false),
+                    Beschreibung = table.Column<string>(nullable: true),
+                    EingereichtAm = table.Column<DateTime>(nullable: false),
+                    Erledigt = table.Column<bool>(nullable: false),
+                    EinreicherId = table.Column<long>(nullable: false),
+                    UebernommenId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aufgabe", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Aufgabe_AspNetUsers_EinreicherId",
+                        column: x => x.EinreicherId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Aufgabe_AspNetUsers_UebernommenId",
+                        column: x => x.UebernommenId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aufgabe_EinreicherId",
+                table: "Aufgabe",
+                column: "EinreicherId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aufgabe_UebernommenId",
+                table: "Aufgabe",
+                column: "UebernommenId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aufgabe");
+        }
+    }
+}
